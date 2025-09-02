90377c2fa2a387db58aeb5549333450d61796367
murang <ddrddr3@126.com>
2025-09-02 19:33:56 +0800

    客户端读配置

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/LoginUI/LoginUI.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/LoginUI/LoginUI.cs
@@ -1,13 +1,32 @@
-﻿using UnityEngine.UI;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using TEngine;
-using Log = TEngine.Log;
 
 namespace GameLogic
 {
-    [Window(UILayer.UI)]
+    [Window(UILayer.UI,location:"LoginUI")]
     class LoginUI : UIWindow
     {
-        
+        #region 脚本工具生成的代码
+        private InputField _inputAccount;
+        private InputField _inputPassword;
+        private Button _btnLogin;
+        protected override void ScriptGenerator()
+        {
+            _inputAccount = FindChildComponent<InputField>("m_inputAccount");
+            _inputPassword = FindChildComponent<InputField>("m_inputPassword");
+            _btnLogin = FindChildComponent<Button>("m_btnLogin");
+            _btnLogin.onClick.AddListener(OnClickLoginBtn);
+        }
+        #endregion
+
+        #region 事件
+        private void OnClickLoginBtn()
+        {
+            var item = ConfigSystem.Instance.Tables.TbItem.Get(10000);
+            Log.Error(item.Desc);
+        }
+        #endregion
+
     }
 }
-
