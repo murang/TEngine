f78bc0d1f83dc314346797bbcd148ee1654cc9ed
murang <ddrddr3@126.com>
2025-09-22 19:35:18 +0800

    text

---
+++ UnityProject/Assets/Fun/rythem/ShowBeat.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(AudioSource))]
+public class ShowBeat : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public float bpm = 117.45f;
+    public float offset = 2.53f;
+    public SpriteRenderer show;
+    public float showTime = .1f;
+    
+    private float _beatInterval;
+    private AudioSource _audioSource;
+    private bool _isShow;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = audioClip;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _beatInterval = bpm / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_audioSource.isPlaying)
+            {
+                _audioSource.Pause();
+            }
+            else
+            {
+                _audioSource.Play();
+            }
+        }
+
+        if (!_audioSource.isPlaying)
+        {
+            return;
+        }
+        
+        var _dspTime = AudioSettings.dspTime - offset - showTime;
+
+        if (!_isShow && _dspTime%_beatInterval < showTime)
+        {
+            Show();
+        }
+    }
+
+    void Show()
+    {
+        _isShow = true;
+        var seq = DOTween.Sequence();
+        seq.Append(show.DOFade(1, showTime));
+        seq.Append(show.DOFade(0, showTime));
+        seq.Play().OnComplete(()=>{_isShow = false;});
+    }
+}
