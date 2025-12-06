3871e1b0dbea61e63ed2ecc74eaba467b15d7f3f
Rock <799375868@qq.com>
2025-12-06 10:27:26 +0800

    修复UI代码自动生成里的问题
    查询的地址是widget的GameObject地址，创建如果用的是CreateWidgetByType的话，会报错，因为UIBase里没有传string的的CreateWidgetByType
    修改成CreateWidget，是直接传相对地址创建的

--- UnityProject/Assets/Editor/UIScriptGenerator/ScriptGenerator.cs
+++ UnityProject/Assets/Editor/UIScriptGenerator/ScriptGenerator.cs
@@ -271,7 +271,7 @@ namespace TEngine.Editor.UI
                     default:
                         if (isUIWidget)
                         {
-                            strBind.Append($"\t\t\t{varName} = CreateWidgetByPrefab<{componentName}>(\"{varPath}\");\n");
+                            strBind.Append($"\t\t\t{varName} = CreateWidget<{componentName}>(\"{varPath}\");\n");
                         }
                         else
                         {
